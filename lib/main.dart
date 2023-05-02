ea4826bb5920d1b189fbae24a3f00dc64bd18df9
Marcus305 <marcusleandro12357@gmail.com>
2023-05-02 17:09:39 -0300

    On Boarding Screen faltando inserir outras duas telas e sem o index em formato de bolinhas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:beholder_companion/screens/onBoarding/views/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 
-//Somente para teste de commit
-
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: OnBoardingScreen(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -73,28 +76,13 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
