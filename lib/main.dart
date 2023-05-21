3e3b680d4e51641af641205f18fff261a027ca70
Samuel Rocha <samucorocha1@gmail.com>
2023-05-21 14:12:56 -0300

    Config Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,36 @@ import 'package:flutter/material.dart';
 import 'package:beholder_companion/screens/tela_de_login/tela_de_login.dart';
 import 'package:beholder_companion/screens/tela_de_pesquisa//tela_de_pesquisa.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      theme: ThemeData(fontFamily: 'Chivo'),
-      home: const TelaDeCadastro(),
-      //home: TelaDePesquisa(),
-      //
-    ),
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       theme: ThemeData(fontFamily: 'Chivo'),
+//       home: const TelaDeCadastro(),
+//       //home: TelaDePesquisa(),
+//       //
+//     ),
+//   );
+// }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(TestPage());
+}
+
+class TestPage extends StatelessWidget {
+  const TestPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Funcionou!')),
+      ),
+    );
+  }
 }
