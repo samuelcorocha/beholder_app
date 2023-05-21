889326c36ebfd5a69b6d29cc01265709800f57bb
Marcus305 <marcusleandro12357@gmail.com>
2023-05-21 13:59:18 -0300

    Branch sem firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:beholder_companion/screens/onBoarding/views/onboarding_screen.da
 import 'package:flutter/material.dart';
 import 'package:beholder_companion/screens/tela_de_login/tela_de_login.dart';
 import 'package:beholder_companion/screens/tela_de_pesquisa//tela_de_pesquisa.dart';
-
+//somente para commitar
 void main() {
   runApp(
     const MaterialApp(
