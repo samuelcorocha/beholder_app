f0b81dde854aa40ddc6405d14a056b9bdd814f1e
ArthurLanna36 <arthur.galanna36@gmail.com>
2023-05-22 16:44:48 -0300

    Cadastro 1 e Cadastro 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:beholder_companion/screens/tela_de_cadastro/tela_de_cadastro.dart';
+import 'package:beholder_companion/screens/tela_de_cadastro/tela_de_cadastro_1.dart';
+import 'package:beholder_companion/screens/tela_de_cadastro/tela_de_cadastro_2.dart';
+import 'package:beholder_companion/screens/tela_de_login/nova_tela_de_login.dart';
 import 'package:flutter/material.dart';
 import 'package:beholder_companion/screens/tela_de_login/tela_de_login.dart';
 import 'package:beholder_companion/screens/tela_de_pesquisa//tela_de_pesquisa.dart';
@@ -7,9 +9,7 @@ void main() {
   runApp(
     MaterialApp(
       theme: ThemeData(fontFamily: 'Chivo'),
-      home: const TelaDeCadastro(),
-      //home: TelaDePesquisa(),
-      //
+      home: const TelaDeCadastro1(),
     ),
   );
 }
