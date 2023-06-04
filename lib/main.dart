a396967bc7fead064efd1136706354a237e223e7
ArthurLanna36 <arthur.galanna36@gmail.com>
2023-06-04 14:34:04 -0300

    Nova Tela de Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:beholder_companion/screens/tela_de_login/tela_de_login.dart';
-import 'package:beholder_companion/screens/tela_de_pesquisa//tela_de_pesquisa.dart';
+import 'package:beholder_companion/screens/tela_de_pesquisa/tela_de_pesquisa.dart';
+import 'screens/tela_de_login/nova_tela_de_login.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: TelaDeLogin(),
+      home: NovaTelaDeLogin(),
       //home: TelaDePesquisa(),
       //
     ),
